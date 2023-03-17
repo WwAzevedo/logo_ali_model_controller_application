c908a727668cad8061f248e43c0cc9e889d716b5
ww_azevedo <wesley.wazevedo@fco.net.br>
2023-03-16 22:40:50 -0300

    Configure bearer token auth for each route

--- src/controllers/carsController.js
+++ src/controllers/carsController.js
@@ -1,65 +1,65 @@
-const express = require('express');
-const router = express.Router();
-const CarsModel = require('../models/carsModel');
+const {getAllCars, getCarById, createCar, updateCar, deleteCar} = require('../models/carsModel');
 
-router.get('/', async (req, res) => {
-  try {
-    const cars = await CarsModel.getAllCars();
-    res.status(200).json(cars);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const car = await CarsModel.getCarById(req.params.id);
-    if (!car) {
-      res.status(404).json({ message: 'Car not found' });
-    } else {
-      res.status(200).json(car);
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const { driverId, model, year, color, licensePlate } = req.body;
-    const newCar = await CarsModel.createCar(driverId, model, year, color, licensePlate);
-    res.status(201).json(newCar);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.patch('/:id', async (req, res) => {
-  try {
-    const { id, model, year, color, licensePlate } = req.body;
-    const updatedCar = await CarsModel.updateCar(id, model, year, color, licensePlate);
-    if (!updatedCar) {
-      res.status(404).json({ message: 'Car not found' });
-    } else {
-      res.status(200).json(updatedCar);
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-
-router.delete('/:id', async (req, res) => {
+const carsController = {
+  getAllCars: async (req, res) => {
     try {
-      const deletedCar = await CarModel.deleteCar(req.params.id);
-    if (!deletedCar) {
-      res.status(404).json({ message: 'Car not found' });
-    } else {
-      res.status(200).json({ message: 'Car deleted' });
-        }
+      const cars = await getAllCars();
+      res.status(200).json(cars);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
-});
-      
-module.exports = router;
+  },
+
+  getCarById: async (req, res) => {
+    try {
+      const car = await getCarById(req.params.id);
+      if (!car) {
+        res.status(404).json({ message: 'Car not found' });
+      } else {
+        res.status(200).json(car);
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  createCar: async (req, res) => {
+    try {
+      const { driverId, model, year, color, licensePlate } = req.body;
+      const newCar = await createCar(driverId, model, year, color, licensePlate);
+      res.status(201).json(newCar);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  updateCar: async (req, res) => {
+    try {
+      const { id, model, year, color, licensePlate } = req.body;
+      const updatedCar = await updateCar(id, model, year, color, licensePlate);
+      if (!updatedCar) {
+        res.status(404).json({ message: 'Car not found' });
+      } else {
+        res.status(200).json(updatedCar);
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  deleteCar: async (req, res) => {
+    try {
+      const deletedCar = await deleteCar(req.params.id);
+      if (!deletedCar) {
+        res.status(404).json({ message: 'Car not found' });
+      } else {
+        res.status(200).json({ message: 'Car deleted' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+};
+
+
+module.exports = carsController;
--- src/controllers/reviewsController.js
+++ src/controllers/reviewsController.js
@@ -1,65 +1,64 @@
-const express = require('express');
-const router = express.Router();
-const ReviewsModel = require('../models/reviewsModel');
+const { getAllReviews, getReviewById, createReview, updateReview, deleteReview } = require('../models/reviewsModel');
 
-router.get('/', async (req, res) => {
-  try {
-    const reviews = await ReviewsModel.getAllReviews();
-    res.status(200).json(reviews);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const review = await ReviewsModel.getReviewById(req.params.id);
-    if (!review) {
-      res.status(404).json({ message: 'Review not found' });
-    } else {
-      res.status(200).json(review);
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const { tripId, reviewerId, reviewedUserId, score, comment } = req.body;
-    const newReview = await ReviewsModel.createReview(tripId, reviewerId, reviewedUserId, score, comment);
-    res.status(201).json(newReview);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.patch('/:id', async (req, res) => {
-  try {
-    const { id, tripId, reviewerId, reviewedUserId, score, comment } = req.body;
-    const updatedReview = await ReviewsModel.updateReview(id, tripId, reviewerId, reviewedUserId, score, comment);
-    if (!updatedReview) {
-      res.status(404).json({ message: 'Review not found' });
-    } else {
-      res.status(200).json(updatedReview);
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-
-router.delete('/:id', async (req, res) => {
+const reviewsController = {
+  async getAllReviews(req, res) {
     try {
-      const deletedReview = await ReviewsModel.deleteReview(req.params.id);
-    if (!deletedReview) {
-      res.status(404).json({ message: 'Review not found' });
-    } else {
-      res.status(200).json({ message: 'Review deleted' });
-        }
+      const reviews = await getAllReviews();
+      res.status(200).json(reviews);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
-});
-      
-module.exports = router;
+  },
+
+  async getReviewById(req, res) {
+    try {
+      const review = await getReviewById(req.params.id);
+      if (!review) {
+        res.status(404).json({ message: 'Review not found' });
+      } else {
+        res.status(200).json(review);
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  async createReview(req, res) {
+    try {
+      const { tripId, reviewerId, reviewedUserId, score, comment } = req.body;
+      const newReview = await createReview(tripId, reviewerId, reviewedUserId, score, comment);
+      res.status(201).json(newReview);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  async updateReview(req, res) {
+    try {
+      const { id, tripId, reviewerId, reviewedUserId, score, comment } = req.body;
+      const updatedReview = await updateReview(id, tripId, reviewerId, reviewedUserId, score, comment);
+      if (!updatedReview) {
+        res.status(404).json({ message: 'Review not found' });
+      } else {
+        res.status(200).json(updatedReview);
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  async deleteReview(req, res) {
+    try {
+      const deletedReview = await deleteReview(req.params.id);
+      if (!deletedReview) {
+        res.status(404).json({ message: 'Review not found' });
+      } else {
+        res.status(200).json({ message: 'Review deleted' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+};
+
+module.exports = reviewsController;
